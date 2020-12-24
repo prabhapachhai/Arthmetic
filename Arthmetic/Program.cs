e59a2f55dffd163be57a07116636e5564653d012
prabhapachhai <pachhaiprabha5727@gmail.com>
2020-12-24 13:28:24 -0600

    initial commit

---
+++ Arthmetic/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace Arthmetic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape.AreaOfCircle(3.14);
+            int x = 6;
+            int y = 5;
+            int z = (x - y);
+            Console.WriteLine("value of z is:" + z);
+            Console.WriteLine("multifications of x*y:" + (x * y));
+        }
+    }
+    //  static void AreaOfCircle(double radius)
+    //{
+    //    double pi = 3.14;
+    //    Console.WriteLine("area of circle is:" + (pi * radius * radius));
+       
+
+
+    //}
+}
---
+++ Arthmetic/Shape.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arthmetic
+{
+    class Shape
+    {
+      public   static void AreaOfCircle(double radius)
+        {
+            double pi = 3.14;
+            Console.WriteLine("area of circle is:" + (pi * radius * radius));
+
+
+
+        }
+    }
+}
